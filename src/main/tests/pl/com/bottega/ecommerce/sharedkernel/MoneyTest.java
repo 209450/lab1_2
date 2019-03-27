f52d93257eaac70e1c925e259a055e3934f251e3
209450 <209450@edu.p.lodz.pl>
2019-03-27 18:54:46 +0100

    test - Dodawanie 2 różnych walut

--- src/main/tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -1,7 +1,23 @@
 package pl.com.bottega.ecommerce.sharedkernel;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Currency;
+
 import static org.junit.Assert.*;
 
 public class MoneyTest {
 
+    private Money money;
+    private Money otherMoney;
+
+    @Before public void setUp() throws Exception {
+        money = new Money(100, "EUR");
+        otherMoney = new Money(1000, "PL");
+    }
+
+    @Test(expected = IllegalArgumentException.class) public void addIncompatibleCurrency() {
+        money.add(otherMoney);
+    }
 }
