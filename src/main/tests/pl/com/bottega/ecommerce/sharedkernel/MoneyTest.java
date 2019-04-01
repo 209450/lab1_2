606460dc43a7f2e5e55024206aefbc769464693f
209450 <209450@edu.p.lodz.pl>
2019-04-01 13:07:00 +0200

    test - dodawanie pieniędzy w tej samej walucie

--- src/main/tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/tests/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -5,19 +5,29 @@ import org.junit.Test;
 
 import java.util.Currency;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MoneyTest {
 
     private Money money;
     private Money otherMoney;
+    private Money moneySecound;
 
     @Before public void setUp() throws Exception {
         money = new Money(100, "EUR");
+        moneySecound = new Money(10,"EUR");
         otherMoney = new Money(1000, "PL");
     }
 
     @Test(expected = IllegalArgumentException.class) public void addIncompatibleCurrency() {
         money.add(otherMoney);
     }
+
+    @Test public void addMoneyWithCompatibleCurrency() {
+        Money result = money.add(moneySecound);
+        assertThat("110.00 €", is(result.toString()));
+    }
+
+
 }
